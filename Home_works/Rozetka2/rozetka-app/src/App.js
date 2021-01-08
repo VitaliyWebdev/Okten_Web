5af30be7cfcacb7c76c74699501d835058cbdd47
Vitaliy <mv703021@gmail.com>
2021-01-08 14:25:13 +0200

    rozetka without main section

--- Home_works/Rozetka2/rozetka-app/src/App.js
+++ Home_works/Rozetka2/rozetka-app/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div>
       <Header/>
+
+        <div className="d-flex">
+            <Wrapper/>
+            <Main/>
+        </div>
+
+
     </div>
   );
 }
---
+++ Home_works/Rozetka2/rozetka-app/src/components/Wrapper.js
@@ -0,0 +1,176 @@
+import React from 'react'
+import './styles/wrapper.css'
+
+
+
+
+
+export default function Wrapper (){
+    return (
+        <div className="menu-wrapper col-xl-3">
+<ul className="list-box">
+    <li className="list-item"><a href="#" className="wrapper-link"><svg viewBox="0 0 24 24" id="icon-fat-2416" width="25px" height="25px" fill="#adb8c8">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V12H14V2H2ZM1 0C0.447715 0 0 0.447715 0 1V14H16V1C16 0.447715 15.5523 0 15 0H1Z" transform="translate(4 3)"> </path>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V4H18V2H2ZM1 0C0.447715 0 0 0.447715 0 1V5C0 5.55228 0.447715 6 1 6H19C19.5523 6 20 5.55228 20 5V1C20 0.447715 19.5523 0 19 0H1Z" transform="translate(2 15)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(10 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 9)"> </path>
+    </svg>Ноутбуки та комп'ютери <span className="pd-left"><svg viewBox="0 0 6 9" id="icon-fat-chevron" width="12px" height="12px" fill="#adb8c8">
+        <path d="M0 0.7L3.5 4.4L0 8.4L0.8 9L5 4.4L0.8 0L0 0.7Z" transform="translate(0.994141)"> </path>
+    </svg></span></a></li>
+    <li className="list-item"><a href="#" className="wrapper-link"><svg viewBox="0 0 24 24" id="icon-fat-2416" width="25px" height="25px" fill="#adb8c8">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V12H14V2H2ZM1 0C0.447715 0 0 0.447715 0 1V14H16V1C16 0.447715 15.5523 0 15 0H1Z" transform="translate(4 3)"> </path>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V4H18V2H2ZM1 0C0.447715 0 0 0.447715 0 1V5C0 5.55228 0.447715 6 1 6H19C19.5523 6 20 5.55228 20 5V1C20 0.447715 19.5523 0 19 0H1Z" transform="translate(2 15)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(10 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 9)"> </path>
+    </svg>Ноутбуки та комп'ютери <span className="pd-left"><svg viewBox="0 0 6 9" id="icon-fat-chevron" width="12px" height="12px" fill="#adb8c8">
+        <path d="M0 0.7L3.5 4.4L0 8.4L0.8 9L5 4.4L0.8 0L0 0.7Z" transform="translate(0.994141)"> </path>
+    </svg></span></a></li>
+    <li className="list-item"><a href="#" className="wrapper-link"><svg viewBox="0 0 24 24" id="icon-fat-2416" width="25px" height="25px" fill="#adb8c8">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V12H14V2H2ZM1 0C0.447715 0 0 0.447715 0 1V14H16V1C16 0.447715 15.5523 0 15 0H1Z" transform="translate(4 3)"> </path>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V4H18V2H2ZM1 0C0.447715 0 0 0.447715 0 1V5C0 5.55228 0.447715 6 1 6H19C19.5523 6 20 5.55228 20 5V1C20 0.447715 19.5523 0 19 0H1Z" transform="translate(2 15)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(10 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 9)"> </path>
+    </svg>Ноутбуки та комп'ютери <span className="pd-left"><svg viewBox="0 0 6 9" id="icon-fat-chevron" width="12px" height="12px" fill="#adb8c8">
+        <path d="M0 0.7L3.5 4.4L0 8.4L0.8 9L5 4.4L0.8 0L0 0.7Z" transform="translate(0.994141)"> </path>
+    </svg></span></a></li>
+    <li className="list-item"><a href="#" className="wrapper-link"><svg viewBox="0 0 24 24" id="icon-fat-2416" width="25px" height="25px" fill="#adb8c8">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V12H14V2H2ZM1 0C0.447715 0 0 0.447715 0 1V14H16V1C16 0.447715 15.5523 0 15 0H1Z" transform="translate(4 3)"> </path>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V4H18V2H2ZM1 0C0.447715 0 0 0.447715 0 1V5C0 5.55228 0.447715 6 1 6H19C19.5523 6 20 5.55228 20 5V1C20 0.447715 19.5523 0 19 0H1Z" transform="translate(2 15)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(10 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 9)"> </path>
+    </svg>Ноутбуки та комп'ютери <span className="pd-left"><svg viewBox="0 0 6 9" id="icon-fat-chevron" width="12px" height="12px" fill="#adb8c8">
+        <path d="M0 0.7L3.5 4.4L0 8.4L0.8 9L5 4.4L0.8 0L0 0.7Z" transform="translate(0.994141)"> </path>
+    </svg></span></a></li>
+    <li className="list-item"><a href="#" className="wrapper-link"><svg viewBox="0 0 24 24" id="icon-fat-2416" width="25px" height="25px" fill="#adb8c8">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V12H14V2H2ZM1 0C0.447715 0 0 0.447715 0 1V14H16V1C16 0.447715 15.5523 0 15 0H1Z" transform="translate(4 3)"> </path>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V4H18V2H2ZM1 0C0.447715 0 0 0.447715 0 1V5C0 5.55228 0.447715 6 1 6H19C19.5523 6 20 5.55228 20 5V1C20 0.447715 19.5523 0 19 0H1Z" transform="translate(2 15)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(10 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 9)"> </path>
+    </svg>Ноутбуки та комп'ютери <span className="pd-left"><svg viewBox="0 0 6 9" id="icon-fat-chevron" width="12px" height="12px" fill="#adb8c8">
+        <path d="M0 0.7L3.5 4.4L0 8.4L0.8 9L5 4.4L0.8 0L0 0.7Z" transform="translate(0.994141)"> </path>
+    </svg></span></a></li><li className="list-item"><a href="#" className="wrapper-link"><svg viewBox="0 0 24 24" id="icon-fat-2416" width="25px" height="25px" fill="#adb8c8">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V12H14V2H2ZM1 0C0.447715 0 0 0.447715 0 1V14H16V1C16 0.447715 15.5523 0 15 0H1Z" transform="translate(4 3)"> </path>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V4H18V2H2ZM1 0C0.447715 0 0 0.447715 0 1V5C0 5.55228 0.447715 6 1 6H19C19.5523 6 20 5.55228 20 5V1C20 0.447715 19.5523 0 19 0H1Z" transform="translate(2 15)"> </path>
+    <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(10 6)"> </path>
+    <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 6)"> </path>
+    <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 9)"> </path>
+</svg>Ноутбуки та комп'ютери <span className="pd-left"><svg viewBox="0 0 6 9" id="icon-fat-chevron" width="12px" height="12px" fill="#adb8c8">
+        <path d="M0 0.7L3.5 4.4L0 8.4L0.8 9L5 4.4L0.8 0L0 0.7Z" transform="translate(0.994141)"> </path>
+    </svg></span></a></li>
+    <li className="list-item"><a href="#" className="wrapper-link"><svg viewBox="0 0 24 24" id="icon-fat-2416" width="25px" height="25px" fill="#adb8c8">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V12H14V2H2ZM1 0C0.447715 0 0 0.447715 0 1V14H16V1C16 0.447715 15.5523 0 15 0H1Z" transform="translate(4 3)"> </path>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V4H18V2H2ZM1 0C0.447715 0 0 0.447715 0 1V5C0 5.55228 0.447715 6 1 6H19C19.5523 6 20 5.55228 20 5V1C20 0.447715 19.5523 0 19 0H1Z" transform="translate(2 15)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(10 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 9)"> </path>
+    </svg>Ноутбуки та комп'ютери <span className="pd-left"><svg viewBox="0 0 6 9" id="icon-fat-chevron" width="12px" height="12px" fill="#adb8c8">
+        <path d="M0 0.7L3.5 4.4L0 8.4L0.8 9L5 4.4L0.8 0L0 0.7Z" transform="translate(0.994141)"> </path>
+    </svg></span></a></li>
+    <li className="list-item"><a href="#" className="wrapper-link"><svg viewBox="0 0 24 24" id="icon-fat-2416" width="25px" height="25px" fill="#adb8c8">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V12H14V2H2ZM1 0C0.447715 0 0 0.447715 0 1V14H16V1C16 0.447715 15.5523 0 15 0H1Z" transform="translate(4 3)"> </path>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V4H18V2H2ZM1 0C0.447715 0 0 0.447715 0 1V5C0 5.55228 0.447715 6 1 6H19C19.5523 6 20 5.55228 20 5V1C20 0.447715 19.5523 0 19 0H1Z" transform="translate(2 15)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(10 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 9)"> </path>
+    </svg>Ноутбуки та комп'ютери <span className="pd-left"><svg viewBox="0 0 6 9" id="icon-fat-chevron" width="12px" height="12px" fill="#adb8c8">
+        <path d="M0 0.7L3.5 4.4L0 8.4L0.8 9L5 4.4L0.8 0L0 0.7Z" transform="translate(0.994141)"> </path>
+    </svg></span></a></li>
+    <li className="list-item"><a href="#" className="wrapper-link"><svg viewBox="0 0 24 24" id="icon-fat-2416" width="25px" height="25px" fill="#adb8c8">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V12H14V2H2ZM1 0C0.447715 0 0 0.447715 0 1V14H16V1C16 0.447715 15.5523 0 15 0H1Z" transform="translate(4 3)"> </path>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 2V4H18V2H2ZM1 0C0.447715 0 0 0.447715 0 1V5C0 5.55228 0.447715 6 1 6H19C19.5523 6 20 5.55228 20 5V1C20 0.447715 19.5523 0 19 0H1Z" transform="translate(2 15)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(10 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 6)"> </path>
+        <path d="M2 1C2 1.55228 1.55228 2 1 2C0.447715 2 0 1.55228 0 1C0 0.447715 0.447715 0 1 0C1.55228 0 2 0.447715 2 1Z" transform="translate(7 9)"> </path>
+    </svg>Ноутбуки та комп'ютери <span className="pd-left"><svg viewBox="0 0 6 9" id="icon-fat-chevron" width="12px" height="12px" fill="#adb8c8">
+        <path d="M0 0.7L3.5 4.4L0 8.4L0.8 9L5 4.4L0.8 0L0 0.7Z" transform="translate(0.994141)"> </path>
+    </svg></span></a></li>
+
+
+</ul>
+            <div className="main-stores">
+                <h4 className="main-side-bar"> Встановлюйте наші додатки </h4>
+               <div className="google-box d-flex justify-content-center">
+                   <a href="#"><img src="https://xl-static.rozetka.com.ua/assets/img/design/button-google-play-ua.svg" alt="Google play"/></a>
+                   <a href="#"><img src="https://xl-static.rozetka.com.ua/assets/img/design/button-appstore-ua.svg" alt="AppStore"/></a>
+               </div>
+            </div>
+<div className="main-side-barr-shifts">
+    <h4 className="main-side-bar mt-4"> Графік роботи Call-центру </h4>
+   <div className="phones-box d-flex flex-column">
+       <a href="" className="main-support__phone">+380 44 80 89 554</a>
+       <a href="" className="main-support__phone">+380 44 80 89 554</a>
+   </div>
+   <div className="main-support__timing">
+<div className="days">У будні<span className="time-box">з 08:00 до 21:00</span></div>
+<div className="days">Субота<span className="time-box">з 08:00 до 21:00</span></div>
+<div className="days">Неділя<span className="time-box">з 08:00 до 21:00</span></div>
+   </div>
+</div>
+            <div className="main-sidebar__block">
+                <h3 className="main-sidebar__heading"> Ми в соціальних мережах </h3>
+                <ul className="main-socials d-flex">
+                    <li className="main-socials-item"><a href="" className="main-socials-link">
+                        <svg viewBox="0 0 51.97 96.12" id="icon-facebook" width="11px" height="20px" fill="white" className="svg-icons-socials">
+                            <path d="M50,0,37.55,0c-14,0-23.05,9.28-23.05,23.66V34.56H2a2,2,0,0,0-2,2v15.8a2,2,0,0,0,2,2H14.49V94.16a2,2,0,0,0,2,2H32.8a2,2,0,0,0,2-2V54.29H49.42a2,2,0,0,0,2-2V36.52a2,2,0,0,0-2-2H34.76V25.32c0-4.44,1.06-6.7,6.85-6.7H50a2,2,0,0,0,2-2V2A2,2,0,0,0,50,0Z"></path>
+                        </svg>
+                    </a></li>
+                    <li className="main-socials-item"><a href="" className="main-socials-link twiter-back-color">
+                        <svg viewBox="0 0 612 497.12" id="icon-twitter" width="11px" height="20px" fill="white" className="svg-icons-socials">
+                            <path d="M612,58.82a250.71,250.71,0,0,1-72.09,19.77A126.07,126.07,0,0,0,595.1,9.18a253.1,253.1,0,0,1-79.78,30.48A125.64,125.64,0,0,0,301.39,154.12,356.43,356.43,0,0,1,42.64,22.95,125.69,125.69,0,0,0,81.5,190.53a125.56,125.56,0,0,1-56.87-15.76v1.57a125.67,125.67,0,0,0,100.69,123.1,127.17,127.17,0,0,1-33.08,4.4,120.5,120.5,0,0,1-23.63-2.33A125.61,125.61,0,0,0,185.86,388.7,251.89,251.89,0,0,1,29.94,442.35,266.81,266.81,0,0,1,0,440.63a354.88,354.88,0,0,0,192.44,56.49c230.95,0,357.19-191.29,357.19-357.19l-.42-16.25A250.7,250.7,0,0,0,612,58.82Z"></path>
+                        </svg>
+                    </a></li>
+                    <li className="main-socials-item"><a href="" className="main-socials-link youtoobe-back-color">
+                        <svg viewBox="0 0 43.1 32" id="icon-youtube" width="11px" height="20px" fill="white" className="svg-icons-socials">
+                            <path d="M41.9,5.5a5.69,5.69,0,0,0-4.5-4.3A88.93,88.93,0,0,0,21.4,0,90.2,90.2,0,0,0,5.3,1.2,5.55,5.55,0,0,0,.8,5.5,65.9,65.9,0,0,0,0,16,54.4,54.4,0,0,0,.9,26.5a5.69,5.69,0,0,0,4.5,4.3A91.89,91.89,0,0,0,21.5,32a91.89,91.89,0,0,0,16.1-1.2,5.55,5.55,0,0,0,4.5-4.3,76.19,76.19,0,0,0,1-10.5A69.63,69.63,0,0,0,41.9,5.5ZM16,23V9l12.2,7Z"></path>
+                        </svg>
+                    </a></li>
+
+                    <li className="main-socials-item"><a href="" className="main-socials-link youtoobe-back-color">
+                        <svg viewBox="0 0 43.1 32" id="icon-youtube" width="11px" height="20px" fill="white" className="svg-icons-socials">
+                            <path d="M41.9,5.5a5.69,5.69,0,0,0-4.5-4.3A88.93,88.93,0,0,0,21.4,0,90.2,90.2,0,0,0,5.3,1.2,5.55,5.55,0,0,0,.8,5.5,65.9,65.9,0,0,0,0,16,54.4,54.4,0,0,0,.9,26.5a5.69,5.69,0,0,0,4.5,4.3A91.89,91.89,0,0,0,21.5,32a91.89,91.89,0,0,0,16.1-1.2,5.55,5.55,0,0,0,4.5-4.3,76.19,76.19,0,0,0,1-10.5A69.63,69.63,0,0,0,41.9,5.5ZM16,23V9l12.2,7Z"></path>
+                        </svg>
+                    </a></li>
+
+                    <li className="main-socials-item"><a href="" className="main-socials-link youtoobe-back-color">
+                        <svg viewBox="0 0 43.1 32" id="icon-youtube" width="11px" height="20px" fill="white" className="svg-icons-socials">
+                            <path d="M41.9,5.5a5.69,5.69,0,0,0-4.5-4.3A88.93,88.93,0,0,0,21.4,0,90.2,90.2,0,0,0,5.3,1.2,5.55,5.55,0,0,0,.8,5.5,65.9,65.9,0,0,0,0,16,54.4,54.4,0,0,0,.9,26.5a5.69,5.69,0,0,0,4.5,4.3A91.89,91.89,0,0,0,21.5,32a91.89,91.89,0,0,0,16.1-1.2,5.55,5.55,0,0,0,4.5-4.3,76.19,76.19,0,0,0,1-10.5A69.63,69.63,0,0,0,41.9,5.5ZM16,23V9l12.2,7Z"></path>
+                        </svg>
+                    </a></li>
+                </ul>
+            </div>
+            <div className="main-sidebar__block">
+                <h3 className="main-links__heading">Допомога</h3>
+                <ul className="main-links__box">
+                    <li className="main-item"><a href="" className="main-link">Доставка та оплата</a></li>
+                    <li className="main-item"><a href="" className="main-link">Кредит</a></li>
+                    <li className="main-item"><a href="" className="main-link">Гарантія</a></li>
+                    <li className="main-item"><a href="" className="main-link">Повернення товару</a></li>
+                    <li className="main-item"><a href="" className="main-link">Сервісні центри</a></li>
+                    <li className="main-item"><a href="" className="main-link">Відстежити замовлення</a></li>
+                </ul>
+                <h3 className="main-links__heading">Інформація про компанію</h3>
+                <ul className="main-links__box">
+                    <li className="main-item"><a href="" className="main-link">Про нас</a></li>
+                    <li className="main-item"><a href="" className="main-link">Умови використання сайту</a></li>
+                    <li className="main-item"><a href="" className="main-link">Вакансії</a></li>
+                    <li className="main-item"><a href="" className="main-link">Контакти</a></li>
+                </ul>
+                <h3 className="main-links__heading">Сервіси</h3>
+                <ul className="main-links__box">
+                    <li className="main-item"><a href="" className="main-link">Бонусний рахунок</a></li>
+                    <li className="main-item"><a href="" className="main-link">Rozetka Premium</a></li>
+                    <li className="main-item"><a href="" className="main-link">Подарункові сертифікати</a></li>
+                    <li className="main-item"><a href="" className="main-link">Переказ грошей з карти на карту</a></li>
+                    <li className="main-item"><a href="" className="main-link">Тури та відпочинок</a></li>
+                </ul>
+
+
+            </div>
+        </div>
+    );
+}
