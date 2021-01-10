5e6ba02f3c614f5ffae4d64a1b07c63c9d17337c
Vitaliy <mv703021@gmail.com>
2021-01-10 19:39:13 +0200

    rozetka final stage done

--- Home_works/Rozetka2/rozetka-app/src/components/Header.js
+++ Home_works/Rozetka2/rozetka-app/src/components/Header.js
@@ -43,7 +43,14 @@ export default function Header (){
                 </div>
 <div className="header-bottom-line d-flex ">
     <a href="#"><img src="https://xl-static.rozetka.com.ua/assets/img/design/logo_n.svg" alt="rozetka-logo" className='ml-30'/></a>
-    <button className="menu-toggler ml-30">Каталог товарів</button>
+    <button className="menu-toggler ml-30"><svg viewBox="0 0 24 24" id="icon-catalog" height="24px" width="24px" fill="#89929c">
+        <g clip-rule="evenodd" fill-rule="evenodd">
+            <path d="m17 2.75735-4.2427 4.24264 4.2427 4.24261 4.2426-4.24261zm-5.6569 2.82843c-.7811.78104-.7811 2.04738 0 2.82842l4.2426 4.2427c.7811.781 2.0475.781 2.8285 0l4.2426-4.2427c.781-.78104.781-2.04738 0-2.82842l-4.2426-4.24264c-.781-.781048-2.0474-.781048-2.8285 0z"></path>
+            <path d="m7 4h-4c-.55228 0-1 .44772-1 1v4c0 .5523.44772 1 1 1h4c.55228 0 1-.4477 1-1v-4c0-.55228-.44772-1-1-1zm-4-2c-1.65685 0-3 1.34315-3 3v4c0 1.6569 1.34315 3 3 3h4c1.65685 0 3-1.3431 3-3v-4c0-1.65685-1.34315-3-3-3z"></path>
+            <path d="m7 16h-4c-.55228 0-1 .4477-1 1v4c0 .5523.44772 1 1 1h4c.55228 0 1-.4477 1-1v-4c0-.5523-.44772-1-1-1zm-4-2c-1.65685 0-3 1.3431-3 3v4c0 1.6569 1.34315 3 3 3h4c1.65685 0 3-1.3431 3-3v-4c0-1.6569-1.34315-3-3-3z"></path>
+            <path d="m19 16h-4c-.5523 0-1 .4477-1 1v4c0 .5523.4477 1 1 1h4c.5523 0 1-.4477 1-1v-4c0-.5523-.4477-1-1-1zm-4-2c-1.6569 0-3 1.3431-3 3v4c0 1.6569 1.3431 3 3 3h4c1.6569 0 3-1.3431 3-3v-4c0-1.6569-1.3431-3-3-3z"></path>
+        </g>
+    </svg><span className="btn-text">Каталог товарів</span></button>
    <div className="search-box"> <input type="text" placeholder="Введіть назву товару" className="ml-30 search-input"/>
        <button className="find-btn">Знайти</button></div>
     <a href=""><svg className="ml-160" viewBox="0 0 40 40" id="icon-header-compare" fill="#a6a5a5" width="40px" height="34px">
